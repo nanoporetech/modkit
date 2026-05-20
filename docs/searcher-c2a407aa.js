f72abb0b9e175c320b19911badf395fce4506f61
Art Rand <art.rand@nanoporetech.com>
2026-05-20 16:52:34 +0100

    chore(docs): builds docs

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b1d0ec24.js',
+                path_to_root + 'searchindex-ec6434a5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
